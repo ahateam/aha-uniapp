0042b3004a677f65eee102655ff392c9b4d32d82
weiyuan <1655749587@qq.com>
2019-08-28 15:38:21 +0800

    集体小程序

---
+++ jiti-uniapp/pages/index/index.vue
@@ -0,0 +1,112 @@
+<template>
+	<view>
+		<view class="">
+			<uni-notice-bar 
+				show-icon="true" 
+				background-color="#cce0fe"
+				text="[多行] 这是 NoticeBar 通告栏，这是 NoticeBar 通告栏，">
+			</uni-notice-bar>
+		</view>
+		<view class="swiper_box">
+			<swiper class="swiper_style" indicator-dots="true" indicator-color="rgba(255, 255, 255, 0.3)" indicator-active-color="#d7cbd1" autoplay="true">
+				<swiper-item>
+					<image src="../../static/a.jpg"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../static/a.jpg"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../static/a.jpg"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="content">
+			<view class="content_box" @click="toPoll">
+				<view class="content_box_text">我的投票</view>
+			</view>
+			<view class="content_box" style="background-color: #9788ff;">
+				<view class="content_box_text">集体资产</view>
+			</view>
+			<view class="content_box" style="background-color: #fb7eb8;">
+				<view class="content_box_text">我的分红</view>
+			</view>
+			<view class="content_box">
+				<view class="content_box_text">我的机构</view>
+			</view>
+			<view class="content_box" style="background-color: #9788ff;">
+				<view class="content_box_text">我的公告</view>
+			</view>
+			<view class="content_box" style="background-color: #fb7eb8;">
+				<view class="content_box_text">我的审批</view>
+			</view>
+			<view class="content_box">
+				<view class="content_box_text">发起投票</view>
+			</view>
+			<view class="content_box" style="background-color: #9788ff;">
+				<view class="content_box_text">职务管理</view>
+			</view>
+			<view class="content_box" style="background-color: #fb7eb8;">
+				<view class="content_box_text">个人信息</view>
+			</view>
+			<view class="content_box">
+				<view class="content_box_text">更换组织</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniNoticeBar from "@/components/uni-notice-bar/uni-notice-bar.vue"
+	export default {
+		components: {uniNoticeBar},
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			toPoll(){
+				uni.navigateTo({
+					url:"../poll/poll"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	body{
+		background-color: #ecf2fe;
+	}
+	.swiper_style image{
+		width: 100%;
+	}
+	.content{
+		width: 100%;
+		margin-bottom: 33rpx;
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+	}
+	.content_box{
+		width: 162rpx;
+		height: 162rpx;
+		background-color: #6cbaf8;
+		border-radius: 10%;
+		margin: 66rpx auto auto 45rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.content_box_text{
+		width: 81rpx;
+		height: 81rpx;
+		font-size: 32rpx;
+		color: #fff;
+		text-align: center;
+		line-height: 40.5rpx;
+	}
+</style>
---
+++ jiti-uniapp/pages/poll/pollInfo.vue
@@ -0,0 +1,98 @@
+<template>
+	<view>
+		<view class="title">1.投票标题</view>
+		<view class="vote-item">
+			<view class="max_text">最多可以选投: 3个选项</view>
+			<view class="vote-item-status">
+				<text style="color:#40c9c6">表决未完成</text>
+				<!-- <span v-if=false style="color: #909399">表决失效</text>
+				<text v-if=false style="color: #67C23A">表决成功</text>
+				<text v-if=false style="color: #F56C6C">表决失败</text> -->
+			</view>
+		</view>
+		<view class="vote_text">应到总人数：82 人，已参投有效人数：0 人，已投票数：0票，其中弃权人数：0 人，未参投人数：82 人</view>
+		<view class="vote_content">
+			<text>投票内容。。。。。松林镇松林村股份经济合作社章程（草案） 第一章 总则 第一条 为规范本社股份经济合作社(以下简称合作社)的行为，保障合作社及其股东的合法权益，根据《农业农居部 中国人民银行国家 市场监督管理总局关于开展农居集体经济组织登记赋码工作的通知（农经发〔2018〕4号）》和有关法律法规</text>
+		</view>
+		<view class="vote_option">
+			<view class="vote_option_text">选项1</view>
+			<view class="vote_option_text">选项2</view>
+			<view class="vote_option_text">选项2</view>
+			<view class="vote_option_text">选项2</view>
+			<view class="vote_option_text">选项3</view>
+		</view>
+		<view class="vote_btn">
+			<view class="vote_btn_sub">
+				<button type="primary">提交投票</button>
+			</view>
+			<view class="vote_btn_res">
+				<button type="default">报告单</button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.title{
+		width: 98%;
+		border-bottom: 2rpx solid #F5F5F5;
+		margin: 10rpx auto;
+	}
+	.vote-item{
+		width: 98%;
+		height: 40rpx;
+		margin: 10rpx auto;
+		font-size: 24rpx;
+		color: #666;
+		display: flex;
+		justify-content: space-between;
+	}
+	.max_text{
+		width: 300rpx;
+		height: 40rpx;
+	}
+	.vote_text{
+		width: 98%;
+		margin: 40rpx auto;
+		background-color: #fff7cc;
+		border: 2rpx solid ##fceea0;
+		border-radius: 5%;
+		color: #f56723;
+		font-size: 28rpx;
+	}
+	.vote_content{
+		width: 98%;
+		margin: 0 auto;
+		font-size: 32rpx;
+	}
+	.vote_option{
+		width: 80%;
+		margin: 40rpx auto;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.vote_option view{
+		width: 140rpx;
+		height: 88rpx;
+		border: 2rpx solid #44bb00;
+		color: #44bb00;
+		font-size: 32rpx;
+		text-align: center;
+		line-height: 88rpx;
+		margin: 10rpx;
+	}
+	.vote_btn{
+		width: 80%;
+		margin: 40rpx auto;
+		
+	}
+	.vote_btn view{
+		margin-bottom: 40rpx;
+	}
+	
+</style>
