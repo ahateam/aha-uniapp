84ce32a5effb4e9b74f5ad6f97e0d4fe4ad99617
cometang <1403029829@qq.com>
2019-08-29 10:03:43 +0800

    修改脚本命令为全部更新提价 文档实时更新

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,25 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+#更新项目代码
+git pull git@github.com:ahateam/aha-uniapp.git master
+git add -A
+git commit -m 'newNotice'
+git push -f git@github.com:ahateam/aha-uniapp.git master
+
+
+# 进入文档根目录
+cd document
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+
+
 # 生成静态文件
 npm run docs:build
+
 
 
 
