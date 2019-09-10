d8e3611c49a21ff5a10d69cde0f34e5b312ad0c9
cometang <1403029829@qq.com>
2019-09-10 14:16:33 +0800

    新增 二类电商项目

---
+++ aha-shop/pages/index/components/Recommend.vue
@@ -0,0 +1,87 @@
+<template>
+	<view>
+		<!-- 猜你喜欢 -->
+	<!-- 	<view class="f-header m-t">
+			<image src="/static/temp/h1.png"></image>
+			<view class="tit-box">
+				<text class="tit">猜你喜欢</text>
+				<text class="tit2">Guess You Like It</text>
+			</view>
+			<text class="yticon icon-you"></text>
+		</view> -->
+		
+		<view class="guess-section">
+			<view 
+				v-for="(item, index) in goodsList" :key="index"
+				class="guess-item"
+				@click="navToDetailPage(item)"
+			>
+				<view class="image-wrapper">
+					<image :src="item.image" mode="aspectFill"></image>
+				</view>
+				<text class="title clamp">{{item.title}}</text>
+				<text class="price">￥{{item.price}}</text>
+			</view>
+		</view>
+		
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		props:{
+			goodsList:Array
+		},
+		methods: {
+			navToDetailPage(){
+				console.log('1111')
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+/* 猜你喜欢 */
+	.guess-section{
+		display:flex;
+		flex-wrap:wrap;
+		padding: 0 30upx;
+		background: #fff;
+		.guess-item{
+			display:flex;
+			flex-direction: column;
+			width: 48%;
+			padding-bottom: 40upx;
+			&:nth-child(2n+1){
+				margin-right: 4%;
+			}
+		}
+		.image-wrapper{
+			width: 100%;
+			height: 330upx;
+			border-radius: 3px;
+			overflow: hidden;
+			image{
+				width: 100%;
+				height: 100%;
+				opacity: 1;
+			}
+		}
+		.title{
+			font-size: $font-lg;
+			color: $font-color-dark;
+			line-height: 80upx;
+		}
+		.price{
+			font-size: $font-lg;
+			color: $uni-color-primary;
+			line-height: 1;
+		}
+	}
+</style>
---
+++ aha-shop/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		addressData:{},
+	},
+	mutations: {
+		
+	},
+	actions: {
+	
+	}
+})
+
+export default store
