9f0369d4b5d338576d913af2f824d141d2463db5
JXians <you>
2019-09-28 15:53:46 +0800

    newNotice

--- document/docs/jiti-uniapp/sidebar.js
+++ document/docs/jiti-uniapp/sidebar.js
@@ -1,14 +1,15 @@
 module.exports = [
-  '/jiti-uniapp/info/',
-  '/jiti-uniapp/info/frontOnline',
-  '/jiti-uniapp/info/javaOnline',
-   {
-      title:'组件',
-      collapsable: true,
-      children:[
-        '/jiti-uniapp/component/',
-      ]
-    },
+	'/jiti-uniapp/info/',
+	'/jiti-uniapp/info/frontOnline',
+	'/jiti-uniapp/info/javaOnline',
+	'/jiti-uniapp/info/11',
+	{
+		title: '组件',
+		collapsable: true,
+		children: [
+			'/jiti-uniapp/component/',
+		]
+	},
 
-  
+
 ]
