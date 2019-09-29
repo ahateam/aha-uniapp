5e9a3c5a165ef2e7a3fb53a78b324f25988e13e1
cometang <1403029829@qq.com>
2019-09-29 10:52:49 +0800

    111 集体app版本上线完成

--- jiti-uniapp/App.vue
+++ jiti-uniapp/App.vue
@@ -1,11 +1,73 @@
+<template>
+	<view>
+
+	</view>
+</template>
 <script>
 	export default {
-		onLaunch: function() {
-			
+		data() {
+			return {
+				provider: '',
+				show: false,
+				versionObj: {}
+			}
+		},
+		methods: {
+			loadBtn() {
+				if (this.platform == 'android') {
+
+					plus.runtime.openURL(this.versionObj.android);
+				} else if (this.platform == 'ios') {
+					plus.runtime.openURL(this.versionObj.ios);
+				}
+			},
+
+			getVersion() {
+
+
+
+
+				this.$api.getVersion({}, (res) => {
+					let resData = JSON.parse(res.data.c)
+					let platform = uni.getSystemInfoSync().platform
+					if (Number(resData.version) > this.$constData.version) {
+						this.show = true
+						this.versionObj = resData
+						this.platform = platform
+						uni.showLoading({
+						    title: '版本更新，已跳转下载...'
+						});
+						setTimeout(() => {
+							this.loadBtn()
+						}, 1500)
+					}
+
+				})
+			}
 
 		},
+		onLaunch: function() {
+			console.log('111')
+			this.getVersion()
+			// 	uni.getProvider({
+			// 		service: 'oauth',
+			// 		success: (res) => {
+			// 			this.provider = res.provider
+
+			// 			uni.showToast({
+			// 				icon: 'none',
+			// 				title: '111111',
+			// 			})
+
+
+
+			// 		},
+			// 	})
+		},
 		onShow: function() {
-			console.log('App Show')
+
+
+
 		},
 		onHide: function() {
 			console.log('App Hide')
