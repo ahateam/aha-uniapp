3a91df20419352ec6ff725978ba52932d0b72372
cometang <1403029829@qq.com>
2019-09-29 10:58:53 +0800

    新增 更新日志

--- document/docs/jiti-uniapp/sidebar.js
+++ document/docs/jiti-uniapp/sidebar.js
@@ -1,5 +1,12 @@
 module.exports = [
 	'/jiti-uniapp/info/',
+	{
+		title: '版本日志',
+		collapsable: true,
+		children: [
+			'/jiti-uniapp/version/0929',
+		]
+	},
 	'/jiti-uniapp/info/frontOnline',
 	'/jiti-uniapp/info/javaOnline',
 	'/jiti-uniapp/info/javaOnlineRecord',
