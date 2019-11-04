99fa314298f5437332a7cead664950e3096b5b84
cometang <1403029829@qq.com>
2019-11-04 19:58:06 +0800

    1104 20.00

--- durian/commen/url.js
+++ durian/commen/url.js
@@ -6,4 +6,6 @@ const baseUrl = 'http://47.99.212.32/api/jiti';  //正式服请求地址前缀
 
 
 
+
+
 export default baseUrl     
--- durian/pages/login/code.vue
+++ durian/pages/login/code.vue
@@ -1,22 +1,35 @@
 <template>
-	<view class="box">
+	<view >
+		<view class="box" :style="boxBg">
 			
+		</view>
 	</view>
 </template>
 
 <script>
+	
 export default {
 		name:'code',
 		data(){
-			
+			return {
+				boxBg:""
+			}
 		},
-		
+		onLoad() {
+			// const { windowWidth, windowHeight } = uni.getSystemInfoSync();
+			// let boxHeight = windowHeight
+			// this.boxStyle.Height = boxHeight+'px'
+			// this.boxStyle.Width = '100%'
+			this.boxBg = 'background:url('+this.$constData.oss+'/image/codeBG.png)'
+		}
 		
 	}
 </script>
 
 <style scoped lang="scss">
-	.box{
-		
-	}
+		.box{
+			width: 100%;
+			height: 100vh;
+			
+		}
 </style>
