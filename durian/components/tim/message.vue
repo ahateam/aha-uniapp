235eba011d9bb51ecc9ddc8710cb802769bb5f78
cometang <1403029829@qq.com>
2019-12-07 19:17:20 +0800

    1207 tim切换到uniapp页面

---
+++ durian/components/tim/message.vue
@@ -0,0 +1,52 @@
+<template>
+	<view >
+		<view v-if="toUserInfo">
+			<view class="nav-box">
+				<titleItem :name="toUserInfo.name"></titleItem>
+			</view>
+			<view class="message-list">
+				<list></list>
+			</view>
+		</view>
+	</view>
+	
+</template>
+<script>
+	import titleItem from './message/titleItem'
+	import list from './message/list'
+	export default{
+		name:'msssage',
+		data(){
+			return {
+				toUserInfo:'',
+			}
+		},
+		components:{
+			titleItem,
+			list
+		},
+		mounted() {
+			let toUserId = uni.getStorageSync('toUserId')
+			let toUserInfo = this.$commen.getUserInfo(toUserId)
+			uni.setStorageSync('toUserInfo',JSON.stringify(toUserInfo))
+			this.toUserInfo = toUserInfo
+			console.log('----toUserInfo--')
+			console.log(this.toUserInfo)
+		
+	
+		}
+	}
+</script>
+<style scoped lang="scss">
+	.nav-box{
+		position: fixed;
+		top: 40rpx;
+		width: 100%;
+		height: 88rpx;
+		z-index: 100;
+		background: #fff;
+	}
+	.message-list{
+		padding-top: 88rpx;
+	}
+</style>
