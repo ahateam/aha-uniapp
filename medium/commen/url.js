1701e1f1f6f61f7be39adea22eed12e79c01f2f1
425180906 <425180906@qq.com>
2019-12-10 14:13:04 +0800

    12.10

--- medium/commen/url.js
+++ medium/commen/url.js
@@ -1,13 +1,11 @@
 /** 后端接口对接服务器地址*/
 
-const baseUrl = 'http://192.168.1.184:8089/durian';  //本地请求地址前缀
-
-// const baseUrl = 'http://192.168.1.128:8089/durian'
-// const baseUrl = 'http://47.99.209.235/durian' // 测试服
+// const baseUrl = 'http://192.168.1.184:8089/durian'//本地请求地址前缀
+const baseUrl = 'http://47.99.209.235/durian' // 测试服
 
 const ossUrl = ''
 
 export default {
 	baseUrl,
 	ossUrl,
-}     
+}
